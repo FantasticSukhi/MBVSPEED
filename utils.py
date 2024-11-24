4ae5c2691fb170a46bf6aa7749abed3bc5a06756
Blackmamba <122316561+FantasticSukhi@users.noreply.github.com>
2024-11-24 17:28:46 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -13,7 +13,7 @@ def load_plugins(plugin_name):
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
     sys.modules["AltronX.modules." + plugin_name] = load
-    print("Altron has Imported " + plugin_name)
+    print("MBV has Imported " + plugin_name)
 
 async def edit_or_reply(event, text):
     if event.sender_id in SUDO_USERS:
